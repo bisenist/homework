23f193d5e95af572ab3e8a613283441fdca384b5
bisenist <bisenister@mail.ru>
2023-03-09 11:58:32 +0300

    пытаюсь загрузить первое задание

---
+++ Lesson1/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NameHello
+{
+    public class Programm
+    {
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine(name + ", привет!");
+
+        }
+    }
+
+}
+
