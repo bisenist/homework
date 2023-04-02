f9133910453986f9be2464ce23cbe9c57fe4f632
bisenist <bisenister@mail.ru>
2023-04-02 17:40:46 +0300

    Четвертая домашка

---
+++ Lesson4/Homework4/Handler2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    public class Handler2
+    {
+        public void SayAgain()
+        {
+            Console.WriteLine("Быстрее действуй!");
+        }
+    }
+}
